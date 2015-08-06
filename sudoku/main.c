94e0b5ade42c07be07008f88eef84862caa3bb03
David Ciani <dciani@davidciani.com>
2015-08-05 22:57:32 -0700

    Initial Commit

---
+++ sudoku/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sudoku
+//
+//  Created by David Ciani on 8/5/15.
+//  Copyright (c) 2015 David Ciani. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
